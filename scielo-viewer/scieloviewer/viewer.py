91bd088a762cb1611a7ac34ff19c4f0e006f69df
Fabio Batalha <fabiobatalha@gmail.com>
2012-02-14 21:55:14 -0200

    Sample Services

---
+++ scielo-viewer/scieloviewer/viewer.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+import urllib2
+
+class MetadataHarvester(object):
+
+    def __init__(self,request,url=None):
+
+        _tmp = ""
--- scielo-viewer/scieloviewer/views.py
+++ scielo-viewer/scieloviewer/views.py
@@ -1,5 +1,14 @@
 from pyramid.view import view_config
+from pyramid.response import Response
+from .services import ServicesSampler
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
+@view_config(route_name='home', renderer='templates/home.pt')
+def home(request):
     return {'project':'scielo-viewer'}
+
+@view_config(route_name='metadata_delivery', renderer='templates/metadata_delivery.pt')
+def json_metadata_delivery(request):
+    ss = ServicesSampler(request)
+    sample = ss.json_metadata_delivery()
+
+    return {'metadata':sample}
